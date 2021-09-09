640444396013db005729f74d6411bc27a583dc80
LoluDayo <oluwatoni87@gmail.com>
2021-09-09 16:08:05 +0100

    *addede random string genrator

--- routes/auth.js
+++ routes/auth.js
@@ -30,6 +30,7 @@ router.use(session({
 
 
 router.post('/login', (req, res) => {
+   
     console.log(req.session)
     try {
         const { username, password } = req.body;
@@ -50,7 +51,7 @@ router.post('/login', (req, res) => {
                 })
             } else {
 
-                req.session.isAuth = results[0].id;
+                req.session.isAuth = results[0].usrId;
                 req.session.isRole = results[0].role;
                 req.session.isName = results[0].name;
                 return res.render('index', {
@@ -70,7 +71,8 @@ router.post('/login', (req, res) => {
 
 router.post('/addArticle', (req, res) => {
     console.log(req.body);
-
+    postId = randomString(3)
+    console.log(postId)
     const { title, about, body, } = req.body;
 
     db.query('SELECt title FROM articles WHERE title = ?', [title], (error, results) => {
@@ -85,7 +87,7 @@ router.post('/addArticle', (req, res) => {
             });
         }
 
-        db.query('INSERT INTO articles SET ?', { authorName: req.session.isName, authorId: req.session.isAuth, about: about, title: title, body: body }, (error, results) => {
+        db.query('INSERT INTO articles SET ?', { authorName: req.session.isName, authorId: req.session.isAuth, about: about, title: title, body: body, articleId }, (error, results) => {
             if (error) {
                 console.log(error);
             } else {
@@ -100,10 +102,41 @@ router.post('/addArticle', (req, res) => {
 
 })
 
-router.post('/addUser', authC.register);
+router.post('/addUser', (req, res) => {
+    console.log(req.body);
+    usrId = randomString(3)
+    console.log(usrId)
+    const { name, username, password, role } = req.body;
+
+    db.query('SELECt username FROM users WHERE username = ?', [username], (error, results) => {
+
+        if (error) {
+            console.log(error);
+        }
+
+        if (results.length > 0) {
+            return res.render('addUser', {
+                message: 'Username is already in use'
+            });
+        }
+
+        db.query('INSERT INTO users SET ?', { name: name, username: username, password: password, role: role, usrId: usrId }, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(results)
+                return res.render('addUser', {
+                    message: 'User Created'
+                })
+            }
+        })
+    })
+})
+
 
 
 router.get('/logout', (req, res) => {
+    
     req.session.destroy(error => {
         if (error) {
             return res.redirect('/')
@@ -114,4 +147,13 @@ router.get('/logout', (req, res) => {
 
 })
 
+function randomString(n) {
+    charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var Str = '';
+    for (var i = 0; i < n; i++) {
+        var index = Math.floor(Math.random() * charSet.length);
+        Str += charSet.substring(index, index+1);
+    }
+    return Str;
+}
 module.exports = router;
